5f89ec7153a99fb4bb84591a5c116725b8d1965b
samofoke <samofoke@student.wethinkcode.co.za>
2020-10-29 16:58:13 +0200

    a script for C++

---
+++ script.py
@@ -0,0 +1,16 @@
+import subprocess
+
+code = r'''
+#include <iostream>
+using namespae std;
+
+int main()
+{
+    cout<< "hello world\n";
+    return 0;
+}
+'''
+
+file_cpp = "new_file.cpp"
+with open(file_cpp,'w') as FOUT:
+    FOUT.write(code)
