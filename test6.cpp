7bb4d0774b5f2aedea6d2c2391e5c73a4ae35c02
samofoke <samofoke@student.wethinkcode.co.za>
2021-02-15 00:51:07 +0200

    type casting basics

---
+++ test6.cpp
@@ -0,0 +1,16 @@
+#include "header/std_lib_facilities.h"
+
+using namespace std;
+
+int main()
+{
+    int i; 
+    i = 0;
+
+    while (i <= 100)
+    {
+        cout<<"int -> "<<i<<" and sqt -> "<<sqrt(i)<<"\n";
+        i++;
+    }
+    return 0;
+}
---
+++ test7.cpp
@@ -0,0 +1,17 @@
+#include "header/std_lib_facilities.h"
+
+using namespace std;
+
+int main()
+{
+    char alpha;
+
+    alpha = 'a';
+
+    while (alpha <= 'z')
+    {
+        cout<<"the ans is: "<<int(alpha)<<endl;
+        alpha++;
+    }
+    return 0;
+}
