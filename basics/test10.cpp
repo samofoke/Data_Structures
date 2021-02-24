bff27832e66bd52bc444020a3eb97c6907553796
samofoke <samofoke@student.wethinkcode.co.za>
2021-02-24 20:24:59 +0200

    std::vector did a small hack

--- basics/test10.cpp
+++ basics/test10.cpp
@@ -7,12 +7,23 @@ int main()
     vector<int>x = {2, 3, 4, 5, 6};
 
     int i;
+    int y;
 
     i = 0;
+    y = 5;
+    //this doesn't fix the error is just a hack so I will
+    //do some more study to understand why this happens.
     while (i < x[i])
     {
-        cout<<"just testing vector: "<<x[i]<<'\n';
-        i++;
+        if (x[i] == y)
+        {
+            cout<<"I am done and I will wait until I run again.."<<'\n';
+            break;
+        }
+        else
+        {
+            cout<<"just testing vector: "<<x[i++]<<'\n';
+        }
     }
     return(0);
 }
