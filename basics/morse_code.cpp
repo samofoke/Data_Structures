cab2351fa8ec329f32030f7d7bef9fa9b089e989
samofoke <samofoke@student.wethinkcode.co.za>
2021-05-16 16:48:44 +0200

    morse code

--- basics/morse_code.cpp
+++ basics/morse_code.cpp
@@ -4,58 +4,101 @@ using namespace std;
 
 string morse_character(char c)
 {
-    string alpha = "abcdefghijklmnopqrstuvwxyz";
-    string mosrse_text[] = {
-        "*_",
-        "_**",
-        "_*_*",
-        "_**",
-        "*",
-        "**_*",
-        "__*",
-        "****",
-        "**",
-        "*___",
-        "_*_",
-        "*_**",
-        "__",
-        "_*",
-        "___",
-        "*__*",
-        "__*_",
-        "*_*",
-        "***",
-        "_",
-        "**_",
-        "***_",
-        "*__",
-        "_**_",
-        "_*__",
-        "__**"
-    };
-
-    int x = mosrse_text.find(c);
-    if (x != -1)
-    {
-        return alpha[x];
-    }
-    else
-    {
-        return " ";
+ 
+    // refer to the Morse table
+    switch (c) {
+    case 'a':
+        return "_*";
+    case 'b':
+        return "*___";
+    case 'c':
+        return "*_*_";
+    case 'd':
+        return "*__";
+    case 'e':
+        return "_";
+    case 'f':
+        return "__*_";
+    case 'g':
+        return "**_";
+    case 'h':
+        return "____";
+    case 'i':
+        return "__";
+    case 'j':
+        return "_***";
+    case 'k':
+        return "*_*";
+    case 'l':
+        return "_*__";
+    case 'm':
+        return "**";
+    case 'n':
+        return "*_";
+    case 'o':
+        return "***";
+    case 'p':
+        return "_**_";
+    case 'q':
+        return "**_*";
+    case 'r':
+        return "_*_";
+    case 's':
+        return "___";
+    case 't':
+        return "*";
+    case 'u':
+        return "__*";
+    case 'v':
+        return "___*";
+    case 'w':
+        return "_**";
+    case 'x':
+        return "*__*";
+    case 'y':
+        return "*_**";
+    case 'z':
+        return "**__";
+    case '1':
+        return "_****";
+    case '2':
+        return "__***";
+    case '3':
+        return "___**";
+    case '4':
+        return "____*";
+    case '5':
+        return "_____";
+    case '6':
+        return "*____-";
+    case '7':
+        return "**___";
+    case '8':
+        return "***__---";
+    case '9':
+        return "****_";
+    case '0':
+        return "*****";
+    default:
+        cerr << "Found invalid character: " << c << ' '<< std::endl;
+        exit(0);
     }
 }
 
+void morse_code(string str)
+{
+    for (int x = 0; str[x]; x++)
+    {
+        cout << morse_character(str[x]) << endl;
+    }
+}
 int main()
 {
-    string *i;
+    string s;
 
-    scanf("%c", &i);
-    string t = "";
-    for (int x = 0; x < i.length(); x++)
-    {
-        t += morse_character(i[x]);
-    }
-    cout<< t <<endl;
+    cin >> s;
+    cout<< "The given characters are: "<< s << endl;
+    morse_code(s);
     return 0;
 }
 
