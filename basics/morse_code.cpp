594629087a7eb7015484d6dec06add5bc9a1cb44
samofoke <samofoke@student.wethinkcode.co.za>
2021-05-16 16:07:38 +0200

    morse concept

---
+++ basics/morse_code.cpp
@@ -0,0 +1,62 @@
+#include "../header/std_lib_facilities.h"
+
+using namespace std;
+
+string morse_character(char c)
+{
+    string alpha = "abcdefghijklmnopqrstuvwxyz";
+    string mosrse_text[] = {
+        "*_",
+        "_**",
+        "_*_*",
+        "_**",
+        "*",
+        "**_*",
+        "__*",
+        "****",
+        "**",
+        "*___",
+        "_*_",
+        "*_**",
+        "__",
+        "_*",
+        "___",
+        "*__*",
+        "__*_",
+        "*_*",
+        "***",
+        "_",
+        "**_",
+        "***_",
+        "*__",
+        "_**_",
+        "_*__",
+        "__**"
+    };
+
+    int x = mosrse_text.find(c);
+    if (x != -1)
+    {
+        return alpha[x];
+    }
+    else
+    {
+        return " ";
+    }
+}
+
+int main()
+{
+    string i;
+
+    getline(cin, i);
+    string t = "";
+    for (int x = 0; x < i.length(); x++)
+    {
+        t += morse_character(i[x]);
+    }
+    cout<< t <<endl;
+    return 0;
+}
+
+
